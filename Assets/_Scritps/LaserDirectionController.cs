b8526a3cecf9c5655961fab79632b6203d48c0a1
Skillath <xabier.gonzalez.69@gmail.com>
2014-09-22 14:33:13 +0200

    Initial commit

---
+++ Assets/_Scritps/LaserDirectionController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserDirectionController : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector2.up * Time.deltaTime * speed);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 0, 90f + this.transform.rotation.eulerAngles.z);
+        }
+        Debug.Log(this.transform.rotation.eulerAngles.z);
+    }
+}
